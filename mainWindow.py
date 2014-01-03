1d316ace8681bd6463065daa2685732377a0f7ba
verb5 <596076@gmail.com>
2014-01-03 22:42:43 +0200

    Update mainWindow.py

--- mainWindow.py
+++ mainWindow.py
@@ -1,12 +1,3 @@
-# -*- coding: utf-8 -*-
-
-# Form implementation generated from reading ui file 'mainWindow.ui'
-#
-# Created: Tue Aug 20 19:54:53 2013
-#      by: pyside-uic 0.2.14 running on PySide 1.1.2
-#
-# WARNING! All changes made in this file will be lost!
-
 from PySide import QtCore, QtGui
 
 class Ui_Dialog(object):
